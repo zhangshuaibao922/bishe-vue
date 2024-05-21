9f57dbcb92040234c4a534c02cfc567e60227fb0
zhangshuaibao922 <2742520302@qq.com>
2024-05-21 22:46:07 +0800

    毕设done

--- src/views/pages/seeScore.vue
+++ src/views/pages/seeScore.vue
@@ -1,15 +1,15 @@
 <template>
-  <div style="width: 100%;height: 100%;" v-loading="loading">
+  <div v-loading="loading" style="width: 100%;height: 100%;">
     <el-card shadow="never" style="margin-left: 100px;margin-right: 100px;height: 90%;">
-      <el-radio-group v-model="examClass" style="margin-bottom: 10px" size="large" @change="toSelectExam">
+      <el-radio-group v-model="examClass" size="large" style="margin-bottom: 10px" @change="toSelectExam">
         <el-radio-button value="1">测试</el-radio-button>
         <el-radio-button value="2">考试</el-radio-button>
       </el-radio-group>
       <el-table
           :data="tableData"
           border
-          style="width: 100%;"
           height="700px"
+          style="width: 100%;"
       >
         <el-table-column label="课程编号" prop="lessonId"/>
         <el-table-column label="考试名称" prop="examName"/>
@@ -22,19 +22,19 @@
         <el-table-column label="考试日期" prop="examData"/>
         <el-table-column label="考试状态">
           <template #default="scope">
-            <el-text v-if="scope.row.isDelete!==1" type="primary" size="large">正常</el-text>
-            <el-text v-else type="danger" size="large">批阅完成</el-text>
+            <el-text v-if="scope.row.isDelete!==1" size="large" type="primary">正常</el-text>
+            <el-text v-else size="large" type="danger">批阅完成</el-text>
           </template>
         </el-table-column>
         <el-table-column label="操作">
           <template #default="scope">
             <div style="display: flex; justify-content: left;">
-            <el-button v-if="scope.row.isDelete===1"
-                size="small"
-                type="success"
-                @click="toSetScore(scope.row)"
-            >查看成绩
-            </el-button>
+              <el-button v-if="scope.row.isDelete===1"
+                         size="small"
+                         type="success"
+                         @click="toSetScore(scope.row)"
+              >查看成绩
+              </el-button>
             </div>
           </template>
         </el-table-column>
@@ -48,16 +48,19 @@ import {useStudentStore} from '@/stores/counter'
 import {ElMessage} from "element-plus";
 import {querySeeScore, queryAllScore} from "@/request/score/score"
 import router from "@/router";
-interface StudentScoreDto{
+
+interface StudentScoreDto {
   studentId: string,
   studentName: string,
   examId: string,
   totalScore: string,
 }
-interface Modelurl{
+
+interface Modelurl {
   paperClassId: string,
   url: number,
 }
+
 interface Exam {
   id: number,
   examId: string,
@@ -69,26 +72,27 @@ interface Exam {
   modelName: string,
   examData: string,
   isDelete: number,
-  models:Modelurl[],
+  models: Modelurl[],
 }
-const loading=ref(true)
-const examClass=ref('1')
+
+const loading = ref(true)
+const examClass = ref('1')
 const tableData = ref<Exam[]>([]);
 const studentStore = useStudentStore();
-const studentId=ref('');
+const studentId = ref('');
 
-const toSetScore=async (data:Exam)=>{
-  studentId.value='1';
-  if(localStorage.getItem('authorityRole')==='STUDENT'){
-    studentId.value=localStorage.getItem('id');
+const toSetScore = async (data: Exam) => {
+  studentId.value = '1';
+  if (localStorage.getItem('authorityRole') === 'STUDENT') {
+    studentId.value = localStorage.getItem('id');
   }
-  const res=await queryAllScore(
-    data.examId,
-    data.lessonId,
+  const res = await queryAllScore(
+      data.examId,
+      data.lessonId,
       studentId.value,
   );
-  localStorage.setItem('ScoreExamID',data.examId);
-  studentId.value='';
+  localStorage.setItem('ScoreExamID', data.examId);
+  studentId.value = '';
   studentStore.setStudentScoreList(res.data.data);
   router.push("studentScore");
 }
@@ -107,7 +111,7 @@ const getTest = () => {
   }).catch((error) => {
     console.error("出现错误", error);
   })
-  loading.value=false
+  loading.value = false
 }
 onMounted(async () => {
   await getTest();
